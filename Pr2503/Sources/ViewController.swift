07bf08d9610e2b9244ed14e86182f5d84d024c37
aivanov <alex130896@gmail.com>
2023-08-10 23:54:04 +0400

    Corrections

--- Pr2503/Sources/ViewController.swift
+++ Pr2503/Sources/ViewController.swift
@@ -130,10 +130,12 @@ class ViewController: UIViewController {
         }
         passwordTextField.text = password
     }
+}
 
-    // MARK: - Methods
+// MARK: - Brute Force Methods
 
-    func bruteForce(passwordToUnlock: String) {
+extension ViewController {
+    private func bruteForce(passwordToUnlock: String) {
         let allowedCharacters: [String] = String().printable.map { String($0) }
         var password: String = ""
 
@@ -148,28 +150,28 @@ class ViewController: UIViewController {
             self?.passwordTextField.isSecureTextEntry = false
         }
     }
-}
 
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index]) : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    } else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
-        }
+    private func indexOf(character: Character, _ array: [String]) -> Int {
+        return array.firstIndex(of: String(character))!
     }
-    return str
+
+    private func characterAt(index: Int, _ array: [String]) -> Character {
+        return index < array.count ? Character(array[index]) : Character("")
+    }
+
+    private func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+        return str
+    }
 }
